d61a5c57d8114b1eec373aba5da80fd00d69ea92
Clannader <oliver..wu@shijigroup.com>
2020-02-10 18:43:00 +0800

    更新代码

--- apiTest/api/ApiTestSingle.js
+++ apiTest/api/ApiTestSingle.js
@@ -3,6 +3,35 @@
  */
 'use strict'
 
-class ApiTestSingle {
-
+const env = process.env.NODE_ENV || 'dev'
+const config = require('../config/' + env + '.env')
+for (let key in config) {
+    process.env[key] = config[key]
 }
+require('./public/globalServer')
+const Config = require('./public/Config')
+const path = __dirname + '/../config/local.ini'
+CGlobal.env['currentConfig'] = new Config(path, true)
+const ApiTestBase = require('./base/ApiTestBase')
+
+class ApiTestSingle extends ApiTestBase{
+
+    constructor() {
+        super()
+    }
+
+    async start() {
+        const result = await this.api.getHttp()
+            .get('/api/test/network', {}).then(res => {
+                return res
+            }).catch(err => err)
+        console.log(result)
+
+        setTimeout(() => {
+            process.exit(0)
+        }, 500)
+    }
+}
+
+const api = new ApiTestSingle()
+api.start()
