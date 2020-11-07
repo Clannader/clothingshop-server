327ab61e350f070c0d5a6f0e67f4301e7e85df45
Clannader <oliver..wu@shijigroup.com>
2020-11-07 20:06:30 +0800

    新增日志下载接口

--- server/log/ServerLogService.js
+++ server/log/ServerLogService.js
@@ -4,8 +4,8 @@ console.log('require ServerLogService')
 let Utils = require('../util/Utils')
 let fs = require('fs')
 let logpath = process.env.BASE_PATH + 'logs'
-let conn = require('../dao/daoConnection')
-let AdminLog = conn.getEntity('AdminLog')
+// let conn = require('../dao/daoConnection')
+// let AdminLog = conn.getEntity('AdminLog')
 
 function ServerLogService() {
 
@@ -30,39 +30,41 @@ ServerLogService.searchLogs = function (req, res) {
 }
 
 ServerLogService.downloadLogs = function (req, res) {
-  let logname = req.query.logname
-  if (!logname) {
-    return res.send({code: 0, msg: CGlobal.serverLang('日志文件名不能为空!')})
+  let logName = req.body.logName
+  if (!logName) {
+    return res.send({code: 0, msg: CGlobal.serverLang(req.lang, '日志文件名不能为空!'
+          , 'serverLog.logNameIsEmpty')})
   }
   let adminSession = Utils.getAdminSession(req)
   if (!CGlobal.isPermission(adminSession.rights, CGlobal.Rights.ServerLogs.code)) {
-    return res.send({code: 0, msg: CGlobal.serverLang('抱歉,你没有 {0} 权限!', CGlobal.Rights.ServerLogs.code)})
+    return res.send({code: 0, msg: CGlobal.serverLang(req.lang, '抱歉,你没有 [{0}] 权限访问!'
+          , 'admin.noRights', CGlobal.Rights.ServerLogs.code)})
   }
   //TODO 这里做成每次只读10M文件,当前端用户看到底部时,再加载10M
   //使用流来读文件
-  fs.readFile(logpath + '/' + logname, function (err, text) {
+  fs.readFile(logpath + '/' + logName, function (err, text) {
     if (err) return res.send({code: 0, msg: err.message})
-    res.send({code: 1, content: text + ''})
+    res.send({code: 1, content: Utils.stringToBase64(text + '')})
   })
 }
 
-ServerLogService.deleteLogs = function (req, res) {
-  let logname = req.body.logname
-  if (!logname) {
-    return res.send({code: 0, msg: CGlobal.serverLang('日志文件名不能为空!')})
-  }
-  let adminSession = Utils.getAdminSession(req)
-  if (!CGlobal.isPermission(adminSession.rights, CGlobal.Rights.ServerLogs.code)) {
-    return res.send({code: 0, msg: CGlobal.serverLang('抱歉,你没有 {0} 权限!', CGlobal.Rights.ServerLogs.code)})
-  }
-  fs.unlink(logpath + '/' + logname, function (err) {
-    if (err) return res.send({code: 0, msg: err.message})
-    AdminLog.createLog({
-      content: CGlobal.serverLang('删除服务器日志:{0}', logname),
-      type: CGlobal.GlobalStatic.Log_Type.SERVER_LOG
-    }, adminSession, CGlobal.noop)
-    res.send({code: 1})
-  })
-}
+// ServerLogService.deleteLogs = function (req, res) {
+//   let logname = req.body.logname
+//   if (!logname) {
+//     return res.send({code: 0, msg: CGlobal.serverLang('日志文件名不能为空!')})
+//   }
+//   let adminSession = Utils.getAdminSession(req)
+//   if (!CGlobal.isPermission(adminSession.rights, CGlobal.Rights.ServerLogs.code)) {
+//     return res.send({code: 0, msg: CGlobal.serverLang('抱歉,你没有 {0} 权限!', CGlobal.Rights.ServerLogs.code)})
+//   }
+//   fs.unlink(logpath + '/' + logname, function (err) {
+//     if (err) return res.send({code: 0, msg: err.message})
+//     AdminLog.createLog({
+//       content: CGlobal.serverLang('删除服务器日志:{0}', logname),
+//       type: CGlobal.GlobalStatic.Log_Type.SERVER_LOG
+//     }, adminSession, CGlobal.noop)
+//     res.send({code: 1})
+//   })
+// }
 
 module.exports = ServerLogService
