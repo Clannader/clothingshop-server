4ba1fb753a32cf7fa3c4c995e06b629fd010c34c
Clannader <oliver..wu@shijigroup.com>
2020-11-23 23:17:54 +0800

    新增swagger功能

---
+++ apiGenerator/api-start.js
@@ -0,0 +1,47 @@
+/**
+ * Create by CC on 2020/11/23
+ * 给vue项目通过swagger专门生成api调用的一个小工具
+ */
+'use strict'
+
+
+// const jsEjs = __dirname + '/template/js.ejs'
+// const apiFile = __dirname + '/api/Login.js'
+// const data = {
+//   isNode: false,
+//   isES6: false,
+//   description: 'Clothes Public API',
+//   isSecure: false,
+//   moduleName: undefined,
+//   className: 'Upload',
+//   imports: undefined,
+//   domain: '',
+//   methods: [
+//     {
+//       path: '/api/file/upload',
+//       className: 'Upload',
+//       methodName: 'uploadFile',
+//       method: 'POST',
+//       isGET: false,
+//       isPOST: true,
+//       summary: '上传文件',
+//       externalDocs: undefined,
+//       isSecure: false,
+//       isSecureToken: false,
+//       isSecureApiKey: false,
+//       isSecureBasic: false,
+//       parameters: [Array],
+//       headers: [Array],
+//       response: 'object',
+//       group: 'File upload api'
+//     }
+//   ],
+//   definitions: [],
+//   apiGroups: { 'File upload api': { desc: 'File upload', items: [Array] } },
+//   defaultDomain: 'http://localhost:8090'
+// }
+//
+// fs.copyTpl(jsEjs, apiFile, data)
+// fs.write("somefile.js", "var a = 1;");
+
+
