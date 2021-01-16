d295708394597bc4b7f9fb4b91b3525e06c135ae
Clannader <oliver..wu@shijigroup.com>
2021-01-16 20:54:00 +0800

    提交代码

--- server/routes/h5Routes/login_route.js
+++ server/routes/h5Routes/login_route.js
@@ -1,16 +1,16 @@
 /**
  * Create by CC on 2018/11/21
  */
-'use strict';
+'use strict'
 
-console.log('require login_route');
-let express = require('express');
-let app = express.Router();
-let cmsLoginService = require('../../h5/CmsLoginService');
+console.log('require login_route')
+let express = require('express')
+let app = express.Router()
+let cmsLoginService = require('../../h5/CmsLoginService')
 
 //给Angular项目和Vue项目调的接口
-app.all('/*',cmsLoginService.checkCors);//前端项目接口API允许跨域访问
-app.post('/user/login', cmsLoginService.userLogin);
-app.post('/user/logout', cmsLoginService.userLogout);
+app.all('/*', cmsLoginService.checkCors)//前端项目接口API允许跨域访问
+app.post('/user/login', cmsLoginService.userLogin)
+app.post('/user/logout', cmsLoginService.userLogout)
 
-module.exports = app;
+module.exports = app
