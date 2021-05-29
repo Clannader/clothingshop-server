3142d99802edba2dae6e3a7b58d32bb4581d6d82
Clannader <oliver..wu@shijigroup.com>
2021-05-29 23:24:06 +0800

    更新前端文件

---
+++ public/static/js/chunk-2d238672.8ac78ea9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d238672"],{feda:function(e,n,t){"use strict";t.r(n);var u={name:"Test"},r=t("2877"),s=Object(r.a)(u,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("router-view")],1)}),[],!1,null,null,null);n.default=s.exports}}]);
